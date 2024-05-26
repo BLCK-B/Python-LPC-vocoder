a7951eb3cd3a011911c4cfb30347796dea5e511f
BLCK <123077751+BLCK-B@users.noreply.github.com>
2024-05-26 19:43:40 +0200

    stereo, sample rates and automatic windowSize

--- main.py
+++ main.py
@@ -1,71 +1,80 @@
-import queue
 import threading
 import time
+
 import librosa
 import numpy as np
 import simpleaudio as sa
-from matplotlib import pyplot as plt
 
-import graphing
-from LPCfun import LPCfun
 from LPCfunOptimized import LPCfunOptimized
-from graphing import graphqueues
-from myFilterIIR import myFilterIIR
 from myFFTfilterIIR import myFFTfilterIIR
 
 
-def play_audio(data, sample_rate):
+def play_stereo(data, sample_rate):
+    if data.shape[0] == 2 and data.shape[1] > data.shape[0]:
+        data = data.T
     # normalize to 16-bit range
     data = data * 32767 / np.max(np.abs(data))
     # convert to 16-bit
     data = data.astype(np.int16)
-    play_obj = sa.play_buffer(data, 1, 2, sample_rate)
+    # ensure the data array is C-contiguous
+    data = data.copy(order='C')
+    play_obj = sa.play_buffer(data, 2, 2, sample_rate)
     play_obj.wait_done()
 
 
-input_path = 'audio/bikes.wav'
-inputc_path = 'audio/raininstr.wav'
+def process_channel(inp, inpc, hannw, p):
+    # voice LPC
+    inp = hannw * inp
+    LPC, _ = LPCfunOptimized(inp, p, False)
+    # carrier LPC
+    _, e = LPCfunOptimized(inpc, p, True)
+    # filter
+    outputCh = hannw[1:] * myFFTfilterIIR(LPC, e)
+    # normalize
+    outputCh = 0.9 * outputCh / np.max(np.abs(outputCh))
+    return outputCh
 
-voiceAudio, fsVoice = librosa.load(input_path)
-carrierAudio, fsCarrier = librosa.load(inputc_path)
-print("sample rate: ", fsVoice)
-# match sample rate
-if fsVoice != fsCarrier:
-    carrierAudio = librosa.resample(carrierAudio, fsCarrier, fsVoice)
 
-windowSize = 4500
+input_path = 'audio/anthr.wav'
+inputc_path = 'audio/test4.wav'
+
+sampleRate = None
+p = 75
+
+voiceAudio, fsVoice = librosa.load(input_path, mono=False, sr=sampleRate)
+carrierAudio, _ = librosa.load(inputc_path, mono=False, sr=fsVoice)
+
+if sampleRate is None:
+    sampleRate = fsVoice
+
+windowSize = round(0.13 * sampleRate / 100) * 100
+print("sample rate: ", fsVoice, "    window size: ", windowSize)
 overlap = 0.5
 overlapSize = int(windowSize * overlap)
 hopSize = windowSize - overlapSize
-windowCount = int(np.floor((len(voiceAudio) - overlapSize) / hopSize))
 
 hann = np.hanning(windowSize)
-carrier = np.zeros(windowSize,)
+
+output = np.zeros((windowSize, 2))
 windowTime = windowSize / fsVoice - overlapSize / fsVoice
 
 count = 0
-for i in range(windowCount):
+for i in range(10000):
     startTime = time.time()
 
     startIndex = i * hopSize
     endIndex = startIndex + windowSize
-    inp = voiceAudio[startIndex:endIndex]
-    inpc = carrierAudio[startIndex:endIndex]
 
-    # voice LPC
-    inp = hann * inp
-    LPC, _ = LPCfunOptimized(inp, 70, False)
+    inpL = voiceAudio[0, startIndex:endIndex]
+    inpR = voiceAudio[1, startIndex:endIndex]
+    inpcL = carrierAudio[0, startIndex:endIndex]
+    inpcR = carrierAudio[1, startIndex:endIndex]
 
-    # carrier LPC
-    _, e = LPCfunOptimized(inpc, 70, True)
-    carrier[1:] = e
+    outputL = process_channel(inpL, inpcL, hann, p)
+    outputR = process_channel(inpR, inpcR, hann, p)
+    output = np.vstack((outputL, outputR))
 
-    # filter
-    output = hann * myFFTfilterIIR(LPC, carrier)
-    # normalize
-    output = 0.9 * output / np.max(np.abs(output))
-
-    t1 = threading.Thread(target=play_audio, args=(output, fsVoice))
+    t1 = threading.Thread(target=play_stereo, args=(output, fsVoice))
     t1.start()
 
     # graphing.b1.extend(inp)
